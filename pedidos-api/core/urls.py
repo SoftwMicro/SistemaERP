2e27df0ee5fa3122a666d3e04a7f467b3c5306c2
Manoel Neto <manoelcontatoneto@gmail.com>
2026-06-06 23:16:11 -0300

    fix: Django urls problemas

--- pedidos-api/core/urls.py
+++ pedidos-api/core/urls.py
@@ -22,6 +22,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('orders.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^api-docs/$', schema_view.with_ui('swagger', cache_timeout=0)),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
